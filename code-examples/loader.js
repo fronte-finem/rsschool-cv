456167dd2af030336a8bcb60f5a7790e8ec775d8
fronte-finem <fronte.finem+github@ya.ru>
2021-03-07 20:07:43 +0300

    feat: add button "close" to modal window

--- code-examples/loader.js
+++ code-examples/loader.js
@@ -11,19 +11,28 @@
     if (!src) throw new Error("No src attribute given.");
     const response = await fetch(src);
     if (response.status !== 200) throw new Error(`Could not load resource: ${src}`);
-    const content = await response.text();
-    elem.innerHTML = content;
+    const html = await response.text();
+    let temp = document.createElement('template');
+    temp.innerHTML = html.trim();
+    elem.appendChild(temp.content.firstChild);
   }
 
   const btnShowJS = cvCode.querySelector('#cv-code-show-js');
   const btnShowPS = cvCode.querySelector('#cv-code-show-ps');
   const btnShowHS = cvCode.querySelector('#cv-code-show-hs');
+  const btnCloseJS = codeExJS.firstElementChild;
+  const btnClosePS = codeExPS.firstElementChild;
+  const btnCloseHS = codeExHS.firstElementChild;
 
-  btnShowJS._code = codeExJS;
-  btnShowPS._code = codeExPS;
-  btnShowHS._code = codeExHS;
+  btnShowJS._code = btnCloseJS._code = codeExJS;
+  btnShowPS._code = btnClosePS._code = codeExPS;
+  btnShowHS._code = btnCloseHS._code = codeExHS;
 
-  for (btn of [btnShowJS, btnShowPS, btnShowHS]) {
-    btn.addEventListener('click', (e) => e.target._code.showModal());
+  for (let btn of [btnShowJS, btnShowPS, btnShowHS]) {
+    btn.addEventListener('click', () => btn._code.showModal());
+  }
+
+  for (let btn of [btnCloseJS, btnClosePS, btnCloseHS]) {
+    btn.addEventListener('click', () => btn._code.close());
   }
 })();
