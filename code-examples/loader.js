fcbf67b8948cfdcd81a1daf8e8a61c405f4e6db4
fronte-finem <fronte.finem+github@ya.ru>
2021-03-07 23:29:59 +0300

    refactor: change implementation of modal window

--- code-examples/loader.js
+++ code-examples/loader.js
@@ -6,33 +6,48 @@
 
   [codeExJS, codeExPS, codeExHS].map(loadContent);
 
-  async function loadContent(elem) {
-    const src = elem.dataset.cvCodeSrc;
+  async function loadContent(temp) {
+    const src = temp.dataset.cvCodeSrc;
     if (!src) throw new Error("No src attribute given.");
     const response = await fetch(src);
     if (response.status !== 200) throw new Error(`Could not load resource: ${src}`);
     const html = await response.text();
-    let temp = document.createElement('template');
     temp.innerHTML = html.trim();
-    elem.appendChild(temp.content.firstChild);
   }
 
   const btnShowJS = cvCode.querySelector('#cv-code-show-js');
   const btnShowPS = cvCode.querySelector('#cv-code-show-ps');
   const btnShowHS = cvCode.querySelector('#cv-code-show-hs');
-  const btnCloseJS = codeExJS.firstElementChild;
-  const btnClosePS = codeExPS.firstElementChild;
-  const btnCloseHS = codeExHS.firstElementChild;
 
-  btnShowJS._code = btnCloseJS._code = codeExJS;
-  btnShowPS._code = btnClosePS._code = codeExPS;
-  btnShowHS._code = btnCloseHS._code = codeExHS;
+  btnShowJS._code = codeExJS;
+  btnShowPS._code = codeExPS;
+  btnShowHS._code = codeExHS;
 
   for (let btn of [btnShowJS, btnShowPS, btnShowHS]) {
-    btn.addEventListener('click', () => btn._code.showModal());
+    btn.addEventListener('click', () => showModal(btn._code));
   }
 
-  for (let btn of [btnCloseJS, btnClosePS, btnCloseHS]) {
-    btn.addEventListener('click', () => btn._code.close());
+  const modal = document.querySelector('#cv-modal');
+
+  modal.querySelector('.cv-modal__close')
+       .addEventListener('click', () => closeModal());
+
+  function showModal(temp) {
+    document.addEventListener('keydown', keydownHandler, false);
+    modal.classList.add('cv-modal--visible');
+    modal.querySelector('.cv-modal__content')
+         .firstElementChild
+         .replaceWith(temp.content.cloneNode(true));
+  }
+
+  function closeModal() {
+    document.removeEventListener('keydown', keydownHandler, false);
+    modal.classList.remove('cv-modal--visible');
+  }
+
+  function keydownHandler(event) {
+    if (event.key === 'Escape' || event.keyCode === 27) {
+      closeModal();
+    }
   }
 })();
